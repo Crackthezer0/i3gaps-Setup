005906fd0ef59baad5b486c988d58cc66986d1b5
caleb <caleb@localhost.localdomain>
2018-12-29 23:26:01 -0500

    New functions and refactoring

--- arch_install.sh
+++ arch_install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 source_building(){
+    #Get everything needed for the installation
+    echo "Installing installation tools"
     sudo pacman --noconfirm --needed -S base-devel git stow && git clone https://aur.archlinux.org/yay.git
     cd yay && makepkg -si
 }
 
 install_packages(){
+    #Install needed packages and do AUR stuff
+    echo "Installing Packages"
     programs=~/dotfiles/i3gaps-Setup/progs.csv
     
     while IFS=, read -r type package description; do
@@ -20,10 +24,20 @@ install_packages(){
 }
 
 vm_tools(){
+    #Tools for running xorg in VirtualBox
+    echo "Getting VM tools:"
     sudo pacman --noconfirm --needed -S virtualbox-guest-utils && \
 	sudo systemctl enable vboxservice.service && \
 	sudo systemctl start vboxservice.service && \
 	VBoxClient-all
+
+post_install_config(){
+    #Change shell and stow dotfiles
+    echo "Starting post configuration:"
+    nwshell=which zsh
+    chsh -s "$nwshell"
+    stow ~/dotfiles/i3gaps-Setup
+}
 
 while getopts "v" o; do
     case $o in
@@ -31,15 +45,9 @@ while getopts "v" o; do
     esac
 done
 
-post_install_config(){
-    chsh -s /usr/bin/zsh
-
-}
-
-
-
 source_building
 
 install_packages
 
-stow ~/dotfiles/i3gaps-Setup
+post_install_config
+
